9d7af8cc06b4dbb1ba3ed4ce650098103cc39f2e
micymike <mosesmichael878@gmail.com>
2025-01-11 14:25:27 +0300

    added vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,23 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: true
+  },
+  optimizeDeps: {
+    include: ['@vitejs/plugin-react']
   },
   server: {
     port: process.env.PORT || 3001,
     host: true
-  },
-  css: {
-    postcss: './postcss.config.cjs'
   }
 });
